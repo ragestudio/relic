96fbae8bc4ab492177ff0439aaecad4c6bcef9a2
srgooglo <srgooglo@ragestudio.net>
2023-11-20 18:31:12 +0100

    added early settings

---
+++ src/renderer/src/pages/settings/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const settingsList = [
+    {
+        id: "drive_auth",
+        name: "Google Drive Authorize",
+        description: "Authorize your Google Drive account to be used for bundles installation.",
+    }
+]
+
+const Settings = () => {
+
+}
+
+export default Settings

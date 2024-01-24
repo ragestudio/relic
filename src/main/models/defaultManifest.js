8129dcd980e1bbe724aa5114ea71b050a7991d05
SrGooglo <srgooglo@ragestudio.net>
2024-01-25 00:38:12 +0100

    added default manifest

---
+++ src/main/models/defaultManifest.js
@@ -0,0 +1,12 @@
+export default {
+    id: null,
+    name: null,
+    description: null,
+    icon: null,
+    version: null,
+    install_path: null,
+    remote_url: null,
+    patches: [],
+    applied_patches: [],
+    options: {},
+}

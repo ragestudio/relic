fd64d244708669d5e06f3ad5031343d5a0458bf5
SrGooglo <srgooglo@ragestudio.net>
2024-02-02 23:38:12 +0100

    improve serialize safe

--- src/main/utils/sendToRender.js
+++ src/main/utils/sendToRender.js
@@ -5,27 +5,35 @@ const forbidden = [
 ]
 
 export default (event, data) => {
-    function serializeIpc(data) {
-        if (!data) {
-            return undefined
+    try {
+        function serializeIpc(data) {
+            if (!data) {
+                return undefined
+            }
+
+            data = JSON.stringify(data)
+
+            data = JSON.parse(data)
+
+            const copy = lodash.cloneDeep(data)
+
+            if (!Array.isArray(copy)) {
+                Object.keys(copy).forEach((key) => {
+                    if (forbidden.includes(key)) {
+                        delete copy[key]
+                    }
+
+                    if (typeof copy[key] === "function") {
+                        delete copy[key]
+                    }
+                })
+            }
+
+            return copy
         }
 
-        const copy = lodash.cloneDeep(data)
-
-        if (!Array.isArray(copy)) {
-            Object.keys(copy).forEach((key) => {
-                if (forbidden.includes(key)) {
-                    delete copy[key]
-                }
-
-                if (typeof copy[key] === "function") {
-                    delete copy[key]
-                }
-            })
-        }
-
-        return copy
+        global.win.webContents.send(event, serializeIpc(data))
+    } catch (error) {
+        console.error(error)
     }
-
-    global.win.webContents.send(event, serializeIpc(data))
 }

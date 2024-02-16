2fa3f10d3ed9ef9d2ea88e674eccb765abc71ef5
SrGooglo <srgooglo@ragestudio.net>
2024-02-16 01:37:31 +0100

    added path_decorators

---
+++ src/renderer/config/paths_decorators.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        path: "/settings",
+        label: "Settings",
+        icon: "MdSettings",
+    },
+    {
+        path: "/pkg/*",
+        label: "Package",
+        icon: "MdPackage",
+    }
+]

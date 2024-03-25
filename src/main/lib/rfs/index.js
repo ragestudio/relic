98401fbf8dd5d56fba71f36fbbe996ce90b1feb8
SrGooglo <srgooglo@ragestudio.net>
2024-03-25 08:13:06 +0100

    cleanup rfs

--- src/main/lib/rfs/index.js
+++ src/main/lib/rfs/index.js
@@ -1,28 +1,8 @@
 import path from "node:path"
 import fs from "node:fs"
 import { execa } from "../../lib/execa"
+
 import Vars from "../../vars"
-
-async function readDirRecurse(dir, maxDepth=3, current = 0) {
-    if (current > maxDepth) {
-        return []
-    }
-
-    const files = await fs.promises.readdir(dir)
-
-    const promises = files.map(async (file) => {
-        const filePath = path.join(dir, file)
-        const stat = await fs.promises.stat(filePath)
-
-        if (stat.isDirectory()) {
-            return readDirRecurse(filePath, maxDepth, current + 1)
-        }
-
-        return filePath
-    })
-
-    return (await Promise.all(promises)).flat()
-}
 
 export default class RFS {
     constructor(manifest) {
@@ -57,15 +37,6 @@ export default class RFS {
             stdout: "inherit",
             stderr: "inherit",
         })
-        
-        await new Promise((r) => {
-            setTimeout(r, 1000)
-        })
-
-        // // try to read from the mount point
-        // let dirs = await readDirRecurse(mountPoint)
-        
-        // console.log(dirs)
 
         if (typeof cb === "function") {
             cb(process)
